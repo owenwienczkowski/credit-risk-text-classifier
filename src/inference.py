a6c5d509f8abfe2ebc730569f29c218fc1031082
Owen Wienczkowski <owenwxyz@gmail.com>
2025-04-23 23:36:14 -0400

    refactored to use pipelines

--- src/inference.py
+++ src/inference.py
@@ -2,7 +2,6 @@ import joblib
 
 def test_logistic_regression(x_test):
     pipe = joblib.load("models/logistic_regression_pipe.pkl")
-    scaled_x_test = pipe.transform(x_test)
-    y_pred = pipe.predict(scaled_x_test)
-    y_prob = pipe.predict_proba(scaled_x_test)
+    y_pred = pipe.predict(x_test)
+    y_prob = pipe.predict_proba(x_test)
     return y_pred, y_prob
