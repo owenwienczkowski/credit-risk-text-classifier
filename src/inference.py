47097345b7baf5ce0933232de14ca4d8389b9577
Owen Wienczkowski <owenwxyz@gmail.com>
2025-04-23 23:21:17 -0400

    refactored to use pipelines

--- src/inference.py
+++ src/inference.py
@@ -1,8 +1,8 @@
 import joblib
 
 def test_logistic_regression(x_test):
-    scaler, model = joblib.load("models/logistic_regression_bundle.pkl")
-    scaled_x_test = scaler.transform(x_test)
-    y_pred = model.predict(scaled_x_test)
-    y_prob = model.predict_proba(scaled_x_test)
+    pipe = joblib.load("models/logistic_regression_pipe.pkl")
+    scaled_x_test = pipe.transform(x_test)
+    y_pred = pipe.predict(scaled_x_test)
+    y_prob = pipe.predict_proba(scaled_x_test)
     return y_pred, y_prob
--- src/train.py
+++ src/train.py
@@ -1,24 +1,27 @@
 import joblib
 from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 
 # function to train a logistic regression model
 def train_logistic_regression(x_train, y_train):
     from sklearn.linear_model import LogisticRegression
 
-    # define model
-    model = LogisticRegression(max_iter=1000, random_state=2025)
-
-    # scale model inputs
-    scaler = StandardScaler()
-    x_train_scaled = scaler.fit_transform(x_train)
+    pipe = build_pipeline(LogisticRegression(max_iter=1000, random_state=2025))
 
     # train model
-    model.fit(x_train_scaled, y_train)
+    pipe.fit(x_train, y_train)
 
     # save model
-    model_path = "models/logistic_regression_bundle.pkl"
-    joblib.dump((scaler, model), model_path)
+    model_path = "models/logistic_regression_pipe.pkl"
+    joblib.dump(pipe, model_path)
 
-    return model
+    return pipe
 
+def build_pipeline(model):
+    # Define pipeline
+    pipe = Pipeline([
+    ('scaler', StandardScaler()),   # define scaler
+    ('classifier', model)      # define model
+    ])
 
+    return pipe
