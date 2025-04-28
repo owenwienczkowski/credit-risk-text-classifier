dbc3ed989bfb1769c1ed7444df068a3ce6f8078f
Owen Wienczkowski <owenwxyz@gmail.com>
2025-04-28 04:11:12 -0400

    tuned gradient boost model

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -12,15 +12,15 @@ if __name__ == "__main__":
         # split into test and training
         x_train, y_train, x_test, y_test = split_data(clean_data)
 
-        # # logistic regression model
-        # logistic_regression_model = train_logistic_regression(x_train, y_train, max_iter=1000, random_state=2025)
-        # y_pred, y_prob = test_logistic_regression(x_test)
-        # evaluate_model(y_pred=y_pred, y_test=y_test, y_prob=y_prob, model_name="log_reg")
+        # logistic regression model
+        logistic_regression_model = train_logistic_regression(x_train, y_train, max_iter=1000, random_state=2025)
+        y_pred, y_prob = test_logistic_regression(x_test)
+        evaluate_model(y_pred=y_pred, y_test=y_test, y_prob=y_prob, model_name="log_reg")
 
-        # # random forest model
-        # random_forest_model = train_random_forest(x_train, y_train, n_estimators=500, random_state=2025)
-        # y_pred_rf, y_prob_rf = test_random_forest(x_test)
-        # evaluate_model(y_pred=y_pred_rf, y_test=y_test, y_prob=y_prob_rf, model_name="random_forest")
+        # random forest model
+        random_forest_model = train_random_forest(x_train, y_train, n_estimators=500, random_state=2025)
+        y_pred_rf, y_prob_rf = test_random_forest(x_test)
+        evaluate_model(y_pred=y_pred_rf, y_test=y_test, y_prob=y_prob_rf, model_name="random_forest")
 
         # gradient boost model
         gradient_boost_model = train_gradient_boost(x_train, y_train, n_estimators=500, #300
